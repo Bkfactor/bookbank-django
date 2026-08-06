4bf9894e715b1c3829906bddc3b483de89288e1d
Benny <erenoseomoh@gmail.com>
2026-08-06 17:29:35 +0100

    fix: remove vercel config to allow zero-config collectstatic

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Installing Python dependencies..."
-python3 -m pip install -r requirements.txt
-
-echo "Collecting static files..."
-python3 manage.py collectstatic --noinput
